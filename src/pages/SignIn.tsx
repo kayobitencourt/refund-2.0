84559ebfe000d6f00ef75623e7698d542aa78acc
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-07-06 20:04:16 -0300

    fix(react): install types for react 19

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,43 +1,24 @@
-import { useState } from "react";
-import { Input } from "../components/Input";
-import { Button } from "../components/Button";
+import { useState } from 'react';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault();
-
-    console.log(email, password);
+  function onAction(formData: FormData) {
+    console.log(formData.get('email'));
   }
 
   return (
-    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-      <Input
-        required
-        legend="E-mail"
-        type="email"
-        placeholder="seu@email.com"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        required
-        legend="Senha"
-        type="password"
-        placeholder="123456"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+    <form action={onAction} className='w-full flex flex-col gap-4'>
+      <Input name='email' required legend='E-mail' type='email' placeholder='seu@email.com' />
+      <Input name='password' required legend='Senha' type='password' placeholder='123456' />
 
-      <Button type="submit" isLoading={isLoading}>
+      <Button type='submit' isLoading={isLoading}>
         Entrar
       </Button>
 
-      <a
-        href="/signup"
-        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
-      >
+      <a href='/signup' className='text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear'>
         Criar conta
       </a>
     </form>
