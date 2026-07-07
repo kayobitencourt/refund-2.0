eb701261aa9a5476fc7e884722939caf0157851c
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-07-07 19:50:35 -0300

    feat(consult): consulting data on input

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -22,7 +22,7 @@ export function Dashboard() {
 
   async function fetchRefunds() {
     try {
-      const response = await api.get<RefundsPaginationAPIResponse>(`/refunds?${name.trim()}&page=${page}&perPage=${PER_PAGE}`);
+      const response = await api.get<RefundsPaginationAPIResponse>(`/refunds?name=${name.trim()}&page=${page}&perPage=${PER_PAGE}`);
 
       setRefunds(
         response.data.refunds.map((refund) => ({
@@ -42,6 +42,11 @@ export function Dashboard() {
         return alert(error.response?.data.message);
       }
     }
+  }
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    fetchRefunds();
   }
 
   function handlePagination(action: 'next' | 'previous') {
@@ -66,7 +71,7 @@ export function Dashboard() {
     <div className='bg-gray-500 rounded-xl p-10 md:min-w-[768px]'>
       <h1 className='text-gray-200 font-bold text-xl flex-1'>Solicitações</h1>
 
-      <form onSubmit={fetchRefunds} className='flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6'>
+      <form onSubmit={onSubmit} className='flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6'>
         <Input placeholder='Pesquisar pelo nome' onChange={(e) => setName(e.target.value)} />
 
         <Button type='submit' variant='icon'>
