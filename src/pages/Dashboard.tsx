d7450e0a39d90536a55b79089ff562b3e8aaff47
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-07-07 19:27:25 -0300

    feat(fetch): fetching refunds on manager dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,41 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
 
-import searchSvg from "../assets/search.svg";
-import { CATEGORIES } from "../utils/categories";
-import { formatCurrency } from "../utils/formatCurrency";
+import { AxiosError } from 'axios';
+import { api } from '../services/api';
 
-import { Input } from "../components/Input";
-import { Button } from "../components/Button";
-import { Pagination } from "../components/Pagination";
-import { RefundItem, RefundItemProps } from "../components/RefundItem";
+import searchSvg from '../assets/search.svg';
+import { CATEGORIES } from '../utils/categories';
+import { formatCurrency } from '../utils/formatCurrency';
+
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+import { Pagination } from '../components/Pagination';
+import { RefundItem, RefundItemProps } from '../components/RefundItem';
 
 const REFUND_EXAMPLE = {
-  id: "123",
-  name: "Rodrigo",
-  category: "Transporte",
+  id: '123',
+  name: 'Rodrigo',
+  category: 'Transporte',
   amount: formatCurrency(34.5),
-  categoryImg: CATEGORIES["transport"].icon,
+  categoryImg: CATEGORIES['transport'].icon,
 };
 
+const PER_PAGE = 5;
+
 export function Dashboard() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [page, setPage] = useState(1);
-  const [totalOfPage, setTotalOfPage] = useState(10);
+  const [totalOfPage, setTotalOfPage] = useState(0);
   const [refunds, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
 
-  function fetchRefunds(e: React.FormEvent) {
-    e.preventDefault();
+  async function fetchRefunds() {
+    try {
+      const response = await api.get(`/refunds?${name.trim()}&page=${page}&perPage=${PER_PAGE}`);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
 
-    console.log(name);
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+    }
   }
 
-  function handlePagination(action: "next" | "previous") {
+  function handlePagination(action: 'next' | 'previous') {
     setPage((prevPage) => {
-      if (action === "next" && prevPage < totalOfPage) {
+      if (action === 'next' && prevPage < totalOfPage) {
         return prevPage + 1;
       }
 
-      if (action === "previous" && prevPage > 1) {
+      if (action === 'previous' && prevPage > 1) {
         return prevPage - 1;
       }
 
@@ -43,40 +55,29 @@ export function Dashboard() {
     });
   }
 
+  useEffect(() => {
+    fetchRefunds();
+  }, []);
+
   return (
-    <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
-      <h1 className="text-gray-200 font-bold text-xl flex-1">Solicitações</h1>
+    <div className='bg-gray-500 rounded-xl p-10 md:min-w-[768px]'>
+      <h1 className='text-gray-200 font-bold text-xl flex-1'>Solicitações</h1>
 
-      <form
-        onSubmit={fetchRefunds}
-        className="flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6"
-      >
-        <Input
-          placeholder="Pesquisar pelo nome"
-          onChange={(e) => setName(e.target.value)}
-        />
+      <form onSubmit={fetchRefunds} className='flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6'>
+        <Input placeholder='Pesquisar pelo nome' onChange={(e) => setName(e.target.value)} />
 
-        <Button type="submit" variant="icon">
-          <img src={searchSvg} alt="Ícone de pesquisar" className="w-5" />
+        <Button type='submit' variant='icon'>
+          <img src={searchSvg} alt='Ícone de pesquisar' className='w-5' />
         </Button>
       </form>
 
-      <div className="my-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
+      <div className='my-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll'>
         {refunds.map((item) => (
-          <RefundItem
-            key={item.id}
-            data={REFUND_EXAMPLE}
-            href={`/refund/${item.id}`}
-          />
+          <RefundItem key={item.id} data={REFUND_EXAMPLE} href={`/refund/${item.id}`} />
         ))}
       </div>
 
-      <Pagination
-        current={page}
-        total={totalOfPage}
-        onNext={() => handlePagination("next")}
-        onPrevious={() => handlePagination("previous")}
-      />
+      <Pagination current={page} total={totalOfPage} onNext={() => handlePagination('next')} onPrevious={() => handlePagination('previous')} />
     </div>
   );
 }
