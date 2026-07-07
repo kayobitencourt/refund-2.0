a8cc2aff95712c930c3cf973ae36f9bf49796a46
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-07-07 17:13:38 -0300

    feat(zod): zod validation

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
-import { useNavigate, useParams } from "react-router";
+import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import { z, ZodError } from 'zod';
 
-import fileSvg from "../assets/file.svg";
-import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
+import fileSvg from '../assets/file.svg';
+import { CATEGORIES, CATEGORIES_KEYS } from '../utils/categories';
 
-import { Input } from "../components/Input";
-import { Select } from "../components/Select";
-import { Upload } from "../components/Upload";
-import { Button } from "../components/Button";
+import { Input } from '../components/Input';
+import { Select } from '../components/Select';
+import { Upload } from '../components/Upload';
+import { Button } from '../components/Button';
+
+const refundSchema = z.object({
+  name: z.string().min(3, { message: 'Informe um nome claro para a sua solicitação' }),
+  category: z.string().min(1, { message: 'Informe a categoria' }),
+  amount: z.coerce.number({ message: 'Informe um valor válido' }).positive({ message: 'Informe um valor válido e superior a 0' }),
+});
 
 export function Refund() {
-  const [name, setName] = useState("Teste");
-  const [amount, setAmount] = useState("34");
-  const [category, setCategory] = useState("transport");
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState('');
+  const [category, setCategory] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [filename, setFilename] = useState<File | null>(null);
 
@@ -26,41 +33,40 @@ export function Refund() {
       return navigate(-1);
     }
 
-    console.log(name, amount, category, filename);
+    try {
+      setIsLoading(true);
 
-    navigate("/confirm", { state: { fromSubmit: true } });
+      const data = refundSchema.parse({
+        name,
+        category,
+        amount: amount.replace(',', '.'),
+      });
+
+      console.log(data);
+      navigate('/confirm', { state: { fromSubmit: true } });
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      alert('Não foi possível realizar a solicitação');
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
-    <form
-      onSubmit={onSubmit}
-      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]"
-    >
+    <form onSubmit={onSubmit} className='bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]'>
       <header>
-        <h1 className="text-xl font-bold text-gray-100">
-          Solicitação de reembolso
-        </h1>
-        <p className="text-sm text-gray-200 mt-2 mb-4">
-          Dados da despesa para solicitar reembolso
-        </p>
+        <h1 className='text-xl font-bold text-gray-100'>Solicitação de reembolso</h1>
+        <p className='text-sm text-gray-200 mt-2 mb-4'>Dados da despesa para solicitar reembolso</p>
       </header>
 
-      <Input
-        required
-        legend="Nome da solicitação"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        disabled={!!params.id}
-      />
+      <Input required legend='Nome da solicitação' value={name} onChange={(e) => setName(e.target.value)} disabled={!!params.id} />
 
-      <div className="flex gap-4">
-        <Select
-          required
-          legend="Categoria"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          disabled={!!params.id}
-        >
+      <div className='flex gap-4'>
+        <Select required legend='Categoria' value={category} onChange={(e) => setCategory(e.target.value)} disabled={!!params.id}>
           {CATEGORIES_KEYS.map((category) => (
             <option key={category} value={category}>
               {CATEGORIES[category].name}
@@ -68,33 +74,20 @@ export function Refund() {
           ))}
         </Select>
 
-        <Input
-          legend="Valor"
-          required
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          disabled={!!params.id}
-        />
+        <Input legend='Valor' required value={amount} onChange={(e) => setAmount(e.target.value)} disabled={!!params.id} />
       </div>
 
       {params.id ? (
-        <a
-          href="https://www.rocketseat.com.br/"
-          target="_black"
-          className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear"
-        >
-          <img src={fileSvg} alt="Ícone do arquivo" />
+        <a href='https://www.rocketseat.com.br/' target='_black' className='text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear'>
+          <img src={fileSvg} alt='Ícone do arquivo' />
           Abrir comprovante
         </a>
       ) : (
-        <Upload
-          filename={filename && filename.name}
-          onChange={(e) => e.target.files && setFilename(e.target.files[0])}
-        />
+        <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])} />
       )}
 
-      <Button type="submit" isLoading={isLoading}>
-        {params.id ? "Voltar" : "Enviar"}
+      <Button type='submit' isLoading={isLoading}>
+        {params.id ? 'Voltar' : 'Enviar'}
       </Button>
     </form>
   );
